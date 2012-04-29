7206394a076ebfad223b056021cb0144808fb6c4
Robert Xu <robxu9@gmail.com>
2012-04-29 01:11:51 -0400

    Implement Lightning and Weather events.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/MQCoreEvents/EnvEvent/LightningEvent.java
+++ src/main/java/com/theminequest/MQCoreEvents/EnvEvent/LightningEvent.java
@@ -1,9 +1,26 @@
 package com.theminequest.MQCoreEvents.EnvEvent;
 
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.LivingEntity;
+
+import com.theminequest.MineQuest.MineQuest;
 import com.theminequest.MineQuest.BukkitEvents.CompleteStatus;
 import com.theminequest.MineQuest.EventsAPI.QEvent;
+import com.theminequest.MineQuest.Quest.Quest;
+import com.theminequest.MineQuest.Target.TargetManager;
 
 public class LightningEvent extends QEvent {
+	
+	private long delay;
+	private long starttime;
+	
+	private boolean targeted;
+	private int targetid;
+	private Location loc;
 
 	public LightningEvent(long q, int e, String details) {
 		super(q, e, details);
@@ -17,25 +34,46 @@ public class LightningEvent extends QEvent {
 	 */
 	@Override
 	public void parseDetails(String[] details) {
-		// TODO Auto-generated method stub
-
+		if (details[0].equalsIgnoreCase("T")){
+			targeted = true;
+			delay = Long.parseLong(details[1]);
+			targetid = Integer.parseInt(details[2]);
+		}else{
+			targeted = false;
+			delay = Long.parseLong(details[0]);
+			World w = Bukkit.getWorld(MineQuest.questManager.getQuest(getQuestId()).getWorld());
+			double x = Double.parseDouble(details[0]);
+			double y = Double.parseDouble(details[1]);
+			double z = Double.parseDouble(details[2]);
+			loc = new Location(w,x,y,z);
+		}
+		starttime = System.currentTimeMillis();
 	}
 
 	@Override
 	public boolean conditions() {
-		// TODO Auto-generated method stub
-		return false;
+		if (starttime+delay>System.currentTimeMillis())
+			return false;
+		return true;
 	}
 
 	@Override
 	public CompleteStatus action() {
-		// TODO Auto-generated method stub
-		return null;
+		Quest q = MineQuest.questManager.getQuest(getQuestId());
+		World w = Bukkit.getWorld(q.getWorld());
+		if (targeted){
+			List<LivingEntity> targets = TargetManager.getTarget(q,q.getTarget(targetid));
+			for (LivingEntity e : targets){
+				w.strikeLightning(e.getLocation());
+			}
+		} else {
+			w.strikeLightning(loc);
+		}
+		return CompleteStatus.SUCCESS;
 	}
 
 	@Override
 	public Integer switchTask() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
--- src/main/java/com/theminequest/MQCoreEvents/EnvEvent/WeatherEvent.java
+++ src/main/java/com/theminequest/MQCoreEvents/EnvEvent/WeatherEvent.java
@@ -1,36 +1,53 @@
 package com.theminequest.MQCoreEvents.EnvEvent;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+import com.theminequest.MineQuest.MineQuest;
 import com.theminequest.MineQuest.BukkitEvents.CompleteStatus;
 import com.theminequest.MineQuest.EventsAPI.QEvent;
+import com.theminequest.MineQuest.Quest.Quest;
 
 public class WeatherEvent extends QEvent {
+	
+	private long delay;
+	private long starttime;
+	
+	private boolean rain;
+	private int duration;
 
 	public WeatherEvent(long q, int e, String details) {
 		super(q, e, details);
 		// TODO Auto-generated constructor stub
 	}
 
+	// Duration is in ticks.
 	@Override
 	public void parseDetails(String[] details) {
-		// TODO Auto-generated method stub
-
+		delay = Long.parseLong(details[0]);
+		rain = Boolean.parseBoolean(details[1]);
+		duration = Integer.parseInt(details[2]);
+		starttime = System.currentTimeMillis();
 	}
 
 	@Override
 	public boolean conditions() {
-		// TODO Auto-generated method stub
-		return false;
+		if (starttime+delay>System.currentTimeMillis())
+			return false;
+		return true;
 	}
 
 	@Override
 	public CompleteStatus action() {
-		// TODO Auto-generated method stub
-		return null;
+		Quest q = MineQuest.questManager.getQuest(getQuestId());
+		World w = Bukkit.getWorld(q.getWorld());
+		w.setStorm(rain);
+		w.setWeatherDuration(duration);
+		return CompleteStatus.SUCCESS;
 	}
 
 	@Override
 	public Integer switchTask() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
